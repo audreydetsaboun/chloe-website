f4fe34e871be2b4543e1bb454bc80af415bf0e73
Audrey Detsaboun <audrey.detsaboun@gmail.com>
2021-02-01 19:26:12 +0100

    résolution du fichier reset non pris en compte ?

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,8 @@ const Header = () => (
     <header>
         <div className="bandeau">
         <img src={telephone} className="tel-logo" alt="tel-logo" />
-        <p>01 43 24 28 80</p>
-        <p>215 av. Pierre Brossolette, 94170 Le Perreux-sur-Marne</p>
+        <p className="tel-number">01 43 24 28 80</p>
+        <p className="adresse">215 av. Pierre Brossolette, 94170 Le Perreux-sur-Marne</p>
         </div>
 
         <h1>Institut Chloé</h1>
